0c58baf60a2b243b50d7a96f1847e2415ba73f59
dandy <dandy03@gmail.com>
2023-09-06 21:04:21 +0800

    commit

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 from claude_api import Client
 
 app = Flask(__name__)
 
-@app.route("/get_user/<user_id>")
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id
-    }
-
-    extra = request.args.get("extra")
-
-    if extra:
-        user_data["extra"] = extra
-
-    return jsonify(user_data), 200
-
-@app.route("/")
-def home():
-    response = {
-        "status": "ok"
-    }
-
+@app.route("/upload", methods=['POST'])
+def upload():
     cookie = request.args.get("cookie")
 
-    if cookie:
-        response["cookie"] = cookie
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
 
+    try:
         claude_api = Client(cookie)
+        prompt = "Hello, Claude! Can you explain this for me?"
+        conversation_id = claude_api.create_new_chat()['uuid']
+        response = claude_api.send_message(prompt, conversation_id, attachment=f.filename, timeout=600)
 
-        conversations = claude_api.list_all_conversations()
+        return jsonify(response), 201
 
-        return conversations, 200
-    else:
-        return jsonify(response), 200
+    except:
+        if conversation_id:
+            claude_api.delete_conversation(conversation_id)
+
+        return {"Error": "Something went wrong, please try again."}, 403
+
+
+# @app.route("/")
+# def home():
+#     response = {
+#         "status": "ok"
+#     }
+#
+#     cookie = request.args.get("cookie")
+#
+#     if cookie:
+#         response["cookie"] = cookie
+#
+#         claude_api = Client(cookie)
+#
+#         conversations = claude_api.list_all_conversations()
+#
+#         return conversations, 200
+#     else:
+#         return jsonify(response), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
